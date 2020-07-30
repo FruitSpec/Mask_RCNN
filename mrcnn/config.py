0a9b9a6a02c8ef54fe6a87eb1c11f4ae3ba824b2
yotamraz2007@gmail.com <r%5p&C7a>
2020-07-30 09:56:26 +0300

    train changes

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -73,7 +73,7 @@ class Config(object):
     NUM_CLASSES = 1  # Override in sub-classes
 
     # Length of square anchor side in pixels
-    RPN_ANCHOR_SCALES = (32, 64, 128, 256, 512)
+    RPN_ANCHOR_SCALES = (16, 32, 64, 128, 256)
 
     # Ratios of anchors at each cell (width/height)
     # A value of 1 represents a square anchor, and 0.5 is a wide anchor
@@ -127,7 +127,7 @@ class Config(object):
     # up scaling. For example, if set to 2 then images are scaled up to double
     # the width and height, or more, even if MIN_IMAGE_DIM doesn't require it.
     # Howver, in 'square' mode, it can be overruled by IMAGE_MAX_DIM.
-    IMAGE_MIN_SCALE = 0
+    IMAGE_MIN_SCALE = 2
 
     # Image mean (RGB)
     MEAN_PIXEL = np.array([98.07, 90.15, 94.86])
