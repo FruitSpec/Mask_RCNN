281276b3f4849081ee491e7bcd13825c6215a19a
yotamraz2007@gmail.com <r%5p&C7a>
2020-08-25 14:34:17 +0300

    clear keras session to free up RAM

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2542,6 +2542,8 @@ class MaskRCNN():
         # Run object detection
         detections, _, _, mrcnn_mask, _, _, _ =\
             self.keras_model.predict([molded_images, image_metas, anchors], verbose=0)
+        # clear session
+        K.clear_session()
         # Process detections
         results = []
         for i, image in enumerate(images):
